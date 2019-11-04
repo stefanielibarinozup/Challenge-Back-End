7c21393087e3abf34cea3665106b6772e1701490
Stefanie Libarino <stefanie.libarino@zup.com.br>
2019-11-04 12:09:42 -0300

    Segundo desafio concluido

--- listartopicos/src/main/java/br/com/zup/challenge/App.java
+++ listartopicos/src/main/java/br/com/zup/challenge/App.java
@@ -4,54 +4,61 @@ import java.net.http.HttpRequest;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class App {
 
     private static final String BASE_URL = "https://www.reddit.com/r/programming/";
+    private static ArrayList<String> listTitle;
 
     public static void main(String[] agrs) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL))
                 .build();
-       HttpResponse<String> response =  client.send(request, HttpResponse.BodyHandlers.ofString());
-            //System.out.println(response.body());
-            filterResult(response.body());
-
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        listTitle = filterResult(response.body());
+        ordenaLista(listTitle);
+        for (String title : ordenaLista(listTitle)) {
+            System.out.println(title);
+        }
     }
 
-     public static void filterResult (String response ) {
-
-        String[] filterResult = response.split(",");
+    public static ArrayList<String> filterResult(String response) {
         int cont = 0;
+        ArrayList<String> listToOrdena = new ArrayList<String>();
+        String[] filterResult = response.split(",");
+        List<String> list = Arrays.asList(filterResult);
+        for (String itemList : list) {
+            if (itemList.length() >= 6) {
+                String itemListItem = itemList.substring(1, 6);
+                if (itemListItem.equals("title") && cont < 20) {
+                    if (!itemList.isEmpty()) {
+                        itemList = itemList.substring(8, itemList.lastIndexOf("\""));
+                        if (itemList.length() > 1) {
+                            listToOrdena.add(itemList);
+                            cont++;
+                        }
+                    }
+                }
+            }
+        }
+        return listToOrdena;
+    }
 
-         List<String> list = Arrays.asList(filterResult);
-
-         for(String itemList : list){
-             if(itemList.length() >= 6){
-                 String itemListItem = itemList.substring(1, 6);
-                 if(itemListItem.equals("title") && cont <20){
-                     cont ++;
-                     itemList = itemList.substring(8, itemList.lastIndexOf("\"") );
-                     //System.out.println(itemList);
-                 }
-             }
-         }
-         int j;
-         int key;
-         int i;
-
-         for (j = 1; j < vetor.length; j++)
-         {
-             key = vetor[j];
-             for (i = j - 1; (i >= 0) && (vetor[i] > key); i--)
-             {
-                 vetor[i + 1] = vetor[i];
-             }
-             vetor[i + 1] = key;
-         }
-
+    public static ArrayList<String> ordenaLista(ArrayList<String> listToOrdena) {
+        int i;
+        String key;
+        int n = listToOrdena.size();
+        for (i = 1; i < n; ++i) {
+            key = listToOrdena.get(i);
+            int j = i - 1;
+            while (j >= 0 && listToOrdena.get(j).compareTo(key) > 0) {
+                listToOrdena.set(j + 1, listToOrdena.get(j));
+                j = j - 1;
+            }
+            listToOrdena.set(j + 1, key);
+        }
+        return listToOrdena;
     }
 }
