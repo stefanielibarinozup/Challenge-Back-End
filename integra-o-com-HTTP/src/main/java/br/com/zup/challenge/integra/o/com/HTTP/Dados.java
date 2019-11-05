e65f5e306cf51035f7565589fd9d728125f66857
Stefanie Libarino <stefanie.libarino@zup.com.br>
2019-11-05 15:04:08 -0300

    Criação do terceiro desafio

---
+++ integra-o-com-HTTP/src/main/java/br/com/zup/challenge/integra/o/com/HTTP/Dados.java
@@ -0,0 +1,12 @@
+package br.com.zup.challenge.integra.o.com.HTTP;
+
+public class Dados {
+    private String id;
+    private String field;
+
+    @Override
+    public String toString() {
+        return "Dados{" + "id:" + id + '\'' + ",field:" + field + '\'' + '}';
+    }
+
+}
---
+++ integra-o-com-HTTP/src/test/java/br/com/zup/challenge/integra/o/com/HTTP/IntegraçãoComHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.challenge.integra.o.com.HTTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegraçãoComHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
